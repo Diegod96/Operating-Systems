3686603e036148eac35a41214ad244db0f7f16c2
Diegod96 <ddphillyfan@gmial.com>
2020-02-03 19:13:01 -0500

    f

--- TreeTraversal/tester.py
+++ TreeTraversal/tester.py
@@ -12,102 +12,102 @@
 #     return characters
 
 
-class Stack(object):
-    def __init__(self):
-        self.stack = []
-
-    def push(self, item):
-        self.stack.append(item)
-
-    def pop(self):
-        return self.stack.pop()
-
-    def peek(self):
-        return self.stack[-1]
-
-    def is_empty(self):
-        return len(self.stack) == 0
-
-
-class Node(object):
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-
-    def __str__(self):
-        return str(self.data)
-
-
-class Tree(object):
-    def __init__(self):
-        self.root = None
-
-    def insert(self, val):
-        new_node = Node(val)
-        if self.root == None:
-            self.root = new_node
-        else:
-            current = self.root
-            parent = self.root
-            while current is not None:
-                parent = current
-                if val < current.data:
-                    current = current.lchild
-                else:
-                    current = current.rchild
-            if val < parent.data:
-                parent.lchild = new_node
-            else:
-                parent.rchild = new_node
-
-    def createTree(self, expr):
-        stack = Stack()
-        operators = ['+', '-', '*', '/', '^']
-        tokens = expr.split()
-        current = Node(None)
-        self.root = current
-        for t in tokens:
-            if t == '(':
-                new_node = Node(None)
-                current.lchild = new_node
-                stack.push(current)
-                current = current.lchild
-            elif t == ')':
-                if not stack.is_empty():
-                    current = stack.pop()
-                else:
-                    return
-            elif t in operators:
-                current.data = t
-                stack.push(current)
-                new_node = Node(None)
-                current.rchild = new_node
-                current = current.rchild
-            else:
-                current.data = t
-                current = stack.pop()
-
-    def preOrder(self, aNode):
-        if aNode is not None:
-            print(aNode, end=' ')
-            self.preOrder(aNode.lchild)
-            self.preOrder(aNode.rchild)
-
-    def postOrder(self, aNode):
-        if aNode is not None:
-            self.postOrder(aNode.lchild)
-            self.postOrder(aNode.rchild)
-            print(aNode, end=' ')
-
-if __name__ == '__main__':
-    expression = input()
-    tree = Tree()
-    tree.createTree(expression)
-    print('Prefix Expression: ', end=' ')
-    tree.preOrder(tree.root)
-    print('Postfix Expression:', end=' ')
-    tree.postOrder(tree.root)
+# class Stack(object):
+#     def __init__(self):
+#         self.stack = []
+#
+#     def push(self, item):
+#         self.stack.append(item)
+#
+#     def pop(self):
+#         return self.stack.pop()
+#
+#     def peek(self):
+#         return self.stack[-1]
+#
+#     def is_empty(self):
+#         return len(self.stack) == 0
+#
+#
+# class Node(object):
+#     def __init__(self, data):
+#         self.data = data
+#         self.left = None
+#         self.right = None
+#
+#     def __str__(self):
+#         return str(self.data)
+#
+#
+# class Tree(object):
+#     def __init__(self):
+#         self.root = None
+#
+#     def insert(self, val):
+#         new_node = Node(val)
+#         if self.root == None:
+#             self.root = new_node
+#         else:
+#             current = self.root
+#             parent = self.root
+#             while current is not None:
+#                 parent = current
+#                 if val < current.data:
+#                     current = current.lchild
+#                 else:
+#                     current = current.rchild
+#             if val < parent.data:
+#                 parent.lchild = new_node
+#             else:
+#                 parent.rchild = new_node
+#
+#     def createTree(self, expr):
+#         stack = Stack()
+#         operators = ['+', '-', '*', '/', '^']
+#         tokens = expr.split()
+#         current = Node(None)
+#         self.root = current
+#         for t in tokens:
+#             if t == '(':
+#                 new_node = Node(None)
+#                 current.lchild = new_node
+#                 stack.push(current)
+#                 current = current.lchild
+#             elif t == ')':
+#                 if not stack.is_empty():
+#                     current = stack.pop()
+#                 else:
+#                     return
+#             elif t in operators:
+#                 current.data = t
+#                 stack.push(current)
+#                 new_node = Node(None)
+#                 current.rchild = new_node
+#                 current = current.rchild
+#             else:
+#                 current.data = t
+#                 current = stack.pop()
+#
+#     def preOrder(self, aNode):
+#         if aNode is not None:
+#             print(aNode, end=' ')
+#             self.preOrder(aNode.lchild)
+#             self.preOrder(aNode.rchild)
+#
+#     def postOrder(self, aNode):
+#         if aNode is not None:
+#             self.postOrder(aNode.lchild)
+#             self.postOrder(aNode.rchild)
+#             print(aNode, end=' ')
+#
+# if __name__ == '__main__':
+#     expression = input()
+#     tree = Tree()
+#     tree.createTree(expression)
+#     print('Prefix Expression: ', end=' ')
+#     tree.preOrder(tree.root)
+#     print('Postfix Expression:', end=' ')
+#     tree.postOrder(tree.root)
 
 
 # class Node:
